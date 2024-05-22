bb568f63715230eb1ca0b416366ea648969c1f25
Harsh0707005 <harshmaster07705@gmail.com>
2024-05-22 09:05:11 +0530

    bug

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Enable CORS for all routes
 app.use(cors());
 
 app.get('/api', async (req, res) => {
